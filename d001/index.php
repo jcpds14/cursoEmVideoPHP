418714dba010322f8804e43645920ada313cfe97
Jean Carlos <jcpds14@gmail.com>
2023-05-17 19:50:03 -0300

    First commit

---
+++ d001/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 001</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Informe um número</h1>
+    </main>
+    <section>
+        <form action="cad.php" method="POST">
+            <label for="numero">Número</label>
+            <input type="number" name="numero" id="idnumero">
+        </form>
+    </section>
+</body>
+
+</html>
