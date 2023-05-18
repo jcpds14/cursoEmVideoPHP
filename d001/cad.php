46a0d8c475db8671c3482b7c07f46c37b37bbd46
Jean Carlos <jcpds14@gmail.com>
2023-05-17 21:29:55 -0300

    Desafio 1

--- d001/cad.php
+++ d001/cad.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado desafio 1</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
@@ -14,7 +15,16 @@
     </main>
     <section>
         <?php 
-            $num = _
+            $num = $_POST["numero"];
+            $numPred = $_POST["numero"] - 1;
+            $numSuce = $_POST["numero"] + 1;
+
+            echo "O número digitado é <strong>$num</strong>";
+            echo "<br>";
+            echo "O predecessor dele é <strong>$numPred</strong>";
+            echo "<br>";
+            echo "O sucessor dele é <strong>$numSuce</strong>";
+            
         ?>
     </section>
 </body>
